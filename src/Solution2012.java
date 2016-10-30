b5b41c05c1bde3a10c33c4035e17015d0a9e2115
pasharik <pasharik@mail.ru>
2016-10-30 18:44:58 +0000

    Task 2012

---
+++ src/Solution2012.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by pasharik on 30.10.16.
+ */
+public class Solution2012 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int f = in.nextInt();
+        if (f >= 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
