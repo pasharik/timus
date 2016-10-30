00a7d1ee7188d0302af69b7b964808e24f36db70
pasharik <pasharik@mail.ru>
2016-10-30 17:50:57 +0000

    Task 1293

---
+++ src/Solution1293.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by pasharik on 30.10.16.
+ */
+public class Solution1293 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(n * a * b * 2);
+    }
+}
